7ad0993f08137f029f87e4c806fb93a1df8eee55
Martin-code13 <mstelo234@gmail.com>
2023-01-18 13:53:55 +0000

    Mi primer commit

---
+++ src/js/component/boostrapcard.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+const BootstrapCard = () => {
+	return (
+        <div class="container">
+		<div class="row">
+			<div class="col-3">
+				<div className="card m-1">
+				<img className="card-img-top" src="https://images.pexels.com/photos/4509351/pexels-photo-4509351.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Card image cap" />
+					<div className="card-body">
+					<h5 className="card-title">1</h5>
+					<p className="card-text">
+						
+					</p>
+					<a href="https://github.com/Martin-code13" className="btn btn-primary">
+						
+					</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<div className="card m-1">
+				<img className="card-img-top" src="https://images.pexels.com/photos/4509351/pexels-photo-4509351.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Card image cap" />
+					<div className="card-body">
+					<h5 className="card-title">2</h5>
+					<p className="card-text">
+                    
+					</p>
+					<a href="https://github.com/Martin-code13" className="btn btn-primary">
+					
+					</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<div className="card m-1">
+				<img className="card-img-top" src="https://images.pexels.com/photos/4509351/pexels-photo-4509351.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Card image cap" />
+					<div className="card-body">
+					<h5 className="card-title">3</h5>
+					<p className="card-text">
+                  
+					</p>
+					<a href="https://github.com/Martin-code13" className="btn btn-primary">
+						
+					</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<div className="card m-1">
+				<img className="card-img-top" src="https://images.pexels.com/photos/4509351/pexels-photo-4509351.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Card image cap" />
+					<div className="card-body">
+					<h5 className="card-title">4</h5>
+					<p className="card-text">
+					
+					</p>
+					<a href="https://github.com/Martin-code13" className="btn btn-primary">
+						
+					</a>
+					</div>
+				</div>
+			</div>
+		</div>
+        </div>
+		);
+};
+
+export default BootstrapCard;
