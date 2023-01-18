59374d101f90e919d66b1700fe08b7bcb189e604
Martin-code13 <mstelo234@gmail.com>
2023-01-18 13:58:15 +0000

    Mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,10 +3,11 @@
 //include images into your bundle
 
 import React from "react";
+import NavBar from "./navbar.jsx";
 import BootstrapCard from "./boostrapcard.jsx";
 import Jumbotron from "./jumbotron.jsx";
 import Footer from "./footer.jsx";
-import NavBar from "./navbar.jsx";
+
 
 //create your first component
 const Home = () => {
